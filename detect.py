4713bd6d153ab4b5d513f0aa5f9b0a2f1b12f5dc
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-22 19:48:39 -0600

    feat: get_gray() - gets the grayscale of an image

--- detect.py
+++ detect.py
@@ -9,6 +9,16 @@ dir_path = os.path.dirname(mypath)
 haarpath = dir_path + "/haarcascades/"
 lbppath = dir_path + "/lbpcascades/"
 
+def get_gray(img):
+    if isinstance(img, classes.EasyImage):
+        img_ref = img.img
+    else:
+        img_ref = cv2.imread(img)
+        if img_ref is None:
+            raise(classes.NotAnImage)
+    gray1 = cv2.cvtColor(img_ref, cv2.COLOR_BGR2GRAY)
+    return gray1
+
 def haarcascades():
     hp = path(haarpath)
     return hp.files()
@@ -18,6 +28,9 @@ def lbpcascasdes():
     return lp.files()
 
 
+    
+    
+
 def using_dlib(img, level = 0):
     """
 Returns a dlib.rectagles object of faces from img input.  The 'img' input can 
@@ -26,13 +39,7 @@ either be a classes.EasyImage object or a string path to an image.
 If you can't find any or all of the faces in an image, try increasing the 
 "level" parameter, although it'll increase execution time.
     """
-    if isinstance(img, classes.EasyImage):
-        img_ref = img.img
-    else:
-        img_ref = cv2.imread(img)
-        if img_ref is None:
-            raise(classes.NotAnImage)
-    gray1 = cv2.cvtColor(img_ref, cv2.COLOR_BGR2GRAY)
+    gray1 = get_gray(img)
     detector = dlib.get_frontal_face_detector()
     return detector(gray1, level)
         
