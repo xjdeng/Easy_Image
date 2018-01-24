10827f38bf9ef65a2642a0fb9f8f572988696113
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-23 22:54:52 -0600

    feat: EasyImage and EasyImageFile
    EasyImage, the base class, just consists of an set of numpy.ndarray pixels
    
    EasyImageFile, the inherited class, is basically an image tha you load from a file.  It retains the path to the file using the self.path member variable.

--- classes.py
+++ classes.py
@@ -4,10 +4,7 @@ import numpy as np
 class EasyImage(object):
     
     def __init__(self, myinput):
-        if isinstance(myinput, str):
-            self.path = str
-            self._img = cv2.imread(myinput)
-        elif isinstance(myinput, np.ndarray):
+        if isinstance(myinput, np.ndarray):
             self.path = None
             self._img = myinput
         else:
@@ -15,7 +12,17 @@ class EasyImage(object):
             
     def getimg(self):
         return self._img
-            
+
+class EasyImageFile(EasyImage):
+    
+    def __init__(self, mypath):
+        if isinstance(mypath, str):
+            self.path = mypath
+            self._img = cv2.imread(mypath)
+        else:
+            raise(NotAnImage)
+    
+                
 
 class NotAnImage(Exception):
     pass
