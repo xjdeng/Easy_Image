c5cda69d8cbc3943de4b6b18f6b724beba03fc48
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-23 22:42:03 -0600

    fix: self.img becomes self._img, a private variable; use self.getimg() instead

--- classes.py
+++ classes.py
@@ -6,12 +6,15 @@ class EasyImage(object):
     def __init__(self, myinput):
         if isinstance(myinput, str):
             self.path = str
-            self.img = cv2.imread(myinput)
+            self._img = cv2.imread(myinput)
         elif isinstance(myinput, np.ndarray):
             self.path = None
-            self.img = myinput
+            self._img = myinput
         else:
             raise(NotAnImage)
+            
+    def getimg(self):
+        return self._img
             
 
 class NotAnImage(Exception):
