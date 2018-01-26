17e0bea9b5808597b1663fe071304bb455b0489e
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-25 21:51:46 -0600

    Add comments to exif_json.py

--- exif_json.py
+++ exif_json.py
@@ -4,8 +4,18 @@ import piexif
 from piexif._exceptions import InvalidImageDataError
 
 exif_field = 40092
+"""
+Uses the XPComment EXIF field to store strings and JSON. If you want to use a
+different field, see: http://www.exiv2.org/tags.html
+"""
 
 def load(img):
+    """
+Note: img is the path to an image, not an EasyImage object!
+    
+Loads the information stored in the img EXIF under the EXIF field specified
+in the variable exif_field into a JSON.
+    """
     try:
         im = Image.open(img)
         raw = im._getexif()[exif_field].decode()
@@ -14,6 +24,12 @@ def load(img):
         return None
 
 def save(img, obj):
+    """
+Note: img is the path to an image, not an EasyImage object!    
+
+Takes a serializable object in obj and stores it in the img under the EXIF
+field specified in exif_field.
+    """
     try:
         json_obj = json.dumps(obj)
         exif_bytes = piexif.dump({"0th":{exif_field:bytes(json_obj,"utf-8")}})
