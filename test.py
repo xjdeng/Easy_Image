4c68852450c5e847cd6d004da94ba8403ccf19b0
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-27 17:22:18 -0600

    fix: get rid of circular dependency between classes.py and detect.py
    All modules in classes.py are merged into detect.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from path import Path as path
 import os,time
 
 try:
-    from . import classes
+    from . import detect
 except ImportError:
-    import classes
+    import detect
 
 mypath = os.path.abspath(__file__)
 dirpath = os.path.dirname(mypath)
@@ -13,7 +13,7 @@ destination_dir = dirpath + "/test_results/"
 
 
 def benchmark(testimgs = testdir, destination = destination_dir, detector = \
-              classes.default_detector):
+              detect.default_detector):
     print("Please don't switch to any other programs while your benchmark is running")
     test_dir = path(testdir)
     destination_dir = path(destination)
@@ -25,25 +25,25 @@ def benchmark(testimgs = testdir, destination = destination_dir, detector = \
     t0 = time.time()
     for f in test_dir.files():
         try:
-            testimg = classes.EasyImageFile(f)
+            testimg = detect.EasyImageFile(f)
             testimg.remove_faces()
             newimg = testimg.draw_faces(detector)
             newimg.save(destination_dir + "/" + str(f.name))
-        except classes.NotAnImage:
+        except detect.NotAnImage:
             pass
     t1 = time.time()
     timefile = open(destination_dir + "/results.txt", 'w')
     timefile.write(str(t1 - t0) + "\n" + str(detector.to_dict()))
 
 def test1():
-    det = classes.detect.DetectorParams('dlib')
+    det = detect.DetectorParams('dlib')
     benchmark(detector=det, destination="test_results2")
 
 def test2():
-    det = classes.detect.DetectorParams('cascade','haar','haarcascade_frontalface_alt2.xml')
+    det = detect.DetectorParams('cascade','haar','haarcascade_frontalface_alt2.xml')
     benchmark(detector=det, destination="test_results2")
     
 def test3():
-    det = classes.detect.DetectorParams('cascade','lbp','lbpcascade_frontalface.xml')
+    det = detect.DetectorParams('cascade','lbp','lbpcascade_frontalface.xml')
     benchmark(detector=det, destination="test_results3")
     
