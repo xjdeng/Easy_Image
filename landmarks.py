55efab2c2314a1a3e9e252454b8a9c54c1dcbd66
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-01-28 10:18:43 -0600

    Add requirements.txt and remove landmarks

--- landmarks.py
+++
@@ -1,9 +0,0 @@
-import dlib, os
-
-
-mypath = os.path.abspath(__file__)
-dir_path = os.path.dirname(mypath)
-predictor = dlib.shape_predictor(dir_path + "/shape_predictor_68_face_landmarks.dat")
-
-def get():
-    return predictor
