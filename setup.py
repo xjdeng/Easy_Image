fc207456358ccd010b25753214ab05ec71246423
xjdeng <xjd001@gmail.com>
2018-03-26 21:11:35 -0500

    Change package name to Easy_Image
    I found myself designing this package more as a general image recognition and classification package rather than a specialized facial detection one so I've decided to change the name.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 #https://docs.python.org/2/distutils/setupscript.html
 #Info on including data files
 
-setup(name="Easy_Facial_Recognition",version='0.1',description='Wrapper\
-      classes to make facial recognition with OpenCV and Dlib easier',\
+setup(name="Easy_Image",version='0.1',description='Wrapper\
+      classes to make facial recognition with OpenCV and Dlib easier and \
+      to automatically detect and classify images.',\
       author='Xiaojian Deng',author_email="xjd001@gmail.com",license='MIT',\
-      packages=['Easy_Facial_Recognition'],include_package_data=True,\
-      package_data={'Easy_Facial_Recognition': ['tests/*','haarcascades/*',\
+      packages=['Easy_Image'],include_package_data=True,\
+      package_data={'Easy_Image': ['tests/*','haarcascades/*',\
                                                 'lbpcascades/*']},
-      url="https://github.com/xjdeng/Easy_Facial_Recognition",zip_safe=False)
+      url="https://github.com/xjdeng/Easy_Image",zip_safe=False)
