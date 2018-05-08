0a5b26431c32d323111efe5b7606a75dce61268e
xjdeng <xjd001@gmail.com>
2018-05-07 20:16:22 -0500

    Add load_dir() to gui

--- Easy_Image/gui.py
+++ Easy_Image/gui.py
@@ -9,3 +9,8 @@ def load():
     imgfile = filedialog.askopenfilename(initialdir = "/",\
                                          title = "Select Image:")
     return detect.EasyImageFile(imgfile)
+
+def load_dir(recursive = False):
+    imgdir = filedialog.askdirectory(initialdir = "/",\
+                                         title = "Select Directory:")
+    return detect.load_image_dir(imgdir, recursive = recursive)
