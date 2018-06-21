72577d5e63ae46ab7bfac05c08e588926c011f24
xjdeng <xjd001@gmail.com>
2018-06-21 12:47:50 -0500

    Add library for making collages

---
+++ Easy_Image/collages.py
@@ -0,0 +1,11 @@
+#WARNING: Under Construction
+
+def make_horizontal(list_of_lists, tgt_w, tgt_h, scheme):
+    """
+    list_of_lists: list of EasyImageLists to pick images from
+    tgt_w: target width (may NOT be exact)
+    tgt_h: target height (will be EXACT)
+    scheme: string or function indicating the scheme for selecting images
+    """
+    pass
+
