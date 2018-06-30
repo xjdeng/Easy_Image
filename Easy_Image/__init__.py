91ba6b5f085954eeb335c947e011ee5f747c8ccf
xjdeng <xjd001@gmail.com>
2018-06-29 22:53:03 -0500

    Import gui in __init__.py

--- Easy_Image/__init__.py
+++ Easy_Image/__init__.py
@@ -3,8 +3,10 @@ try:
     faces_in_dir, load_image
     from . import age_and_gender
     from .detect import NotAnImage, NotFace
+    from . import gui
 except ImportError:
     from detect import DetectorParams, EasyImage, EasyImageFile, EasyFace,\
     faces_in_dir, load_image
     import age_and_gender
     from detect import NotAnImage, NotFace
+    import gui
