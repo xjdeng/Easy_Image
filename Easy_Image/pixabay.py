d113ae151fb45d39f6ea9d1988a98fea429bfa26
xjdeng <xjd001@gmail.com>
2018-07-15 16:19:11 -0500

    Initialize cache

--- Easy_Image/pixabay.py
+++ Easy_Image/pixabay.py
@@ -1,1 +1,16 @@
 import python_pixabay
+import tempfile
+from path import Path as path
+from sklearn.externals import joblib
+
+cache_filename = "pixabay_cache.pkl"
+
+def cache_path():
+    return tempfile.gettempdir() + "/" + cache_filename
+
+try:
+    cache = joblib.load(cache_path())
+    if isinstance(cache, dict) == False:
+        cache = {}
+except IOError:
+    cache = {}
