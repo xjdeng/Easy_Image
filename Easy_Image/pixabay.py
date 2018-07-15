ade3c504e1ec58e9d185a6fdd960c1d6b0404f1e
xjdeng <xjd001@gmail.com>
2018-07-15 17:05:21 -0500

    Make updating the API key into a function

--- Easy_Image/pixabay.py
+++ Easy_Image/pixabay.py
@@ -2,6 +2,8 @@ import python_pixabay
 import tempfile
 from path import Path as path
 from sklearn.externals import joblib
+import os
+import warnings
 
 cache_filename = "pixabay_cache.pkl"
 
@@ -14,3 +16,21 @@ try:
         cache = {}
 except IOError:
     cache = {}
+
+api_key = None
+
+def update_api_key(): 
+    try:
+        api_key = os.environ['PIXABAY_API_KEY']
+    except KeyError:
+        message = """Pixabay API key not found.  Please get your key at 
+https://pixabay.com/api/docs/ and either set it in your OS's PIXABAY_API_KEY 
+environment variable or set it by calling the set_key() function.""".replace("\n","")
+        warnings.warn(message)
+
+def set_key(key):
+    api_key = key
+
+update_api_key()
+
+
