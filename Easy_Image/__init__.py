49723f0a47453e8a269c4010d1993c671dea8f43
xjdeng <xjd001@gmail.com>
2018-08-03 19:51:07 -0500

    Add additional imports to __init__.py

--- Easy_Image/__init__.py
+++ Easy_Image/__init__.py
@@ -1,10 +1,12 @@
 try:
     from .detect import DetectorParams, EasyImage, EasyImageFile, EasyFace,\
-    faces_in_dir, load_image
+    faces_in_dir, load_image, load_image_dir, EasyImageList, \
+    EasyImageFileList, EasyFaceList, ImageFileList
     from .detect import NotAnImage, NotFace
     from . import gui
 except ImportError:
     from detect import DetectorParams, EasyImage, EasyImageFile, EasyFace,\
-    faces_in_dir, load_image
+    faces_in_dir, load_image, load_image_dir, EasyImageList, \
+    EasyImageFileList, EasyFaceList, ImageFileList
     from detect import NotAnImage, NotFace
     import gui
