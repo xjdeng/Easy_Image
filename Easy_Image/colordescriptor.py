c15f8aaad05517d48245379c3bcbc833ed93569a
xjdeng <xjd001@gmail.com>
2018-08-03 19:34:31 -0500

    Unindent chi2_distance

--- Easy_Image/colordescriptor.py
+++ Easy_Image/colordescriptor.py
@@ -103,10 +103,10 @@ class Searcher:
 		# return our (limited) results
 		return results[:limit]
 
-	def chi2_distance(self, histA, histB, eps = 1e-10):
+def chi2_distance(self, histA, histB, eps = 1e-10):
 		# compute the chi-squared distance
-		d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
-			for (a, b) in zip(histA, histB)])
+	d = 0.5 * np.sum([((a - b) ** 2) / (a + b + eps)
+		for (a, b) in zip(histA, histB)])
 
 		# return the chi-squared distance
-		return d
+	return d
