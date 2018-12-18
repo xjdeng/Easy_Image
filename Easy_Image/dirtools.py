ca5df352fca3ec8000ef122b8f6a772d97f4bf16
xjdeng <xjd001@gmail.com>
2018-12-18 17:06:00 -0600

    Add a move() and train_test_split() function

--- Easy_Image/dirtools.py
+++ Easy_Image/dirtools.py
@@ -3,6 +3,7 @@ from path import Path as path
 from PIL import Image
 import random
 
+
 def _isimage(myfile):
     try:
         Image.open(myfile)
@@ -16,6 +17,9 @@ def _norm(mylist):
 
 def copy(mylist, mydest):
     [f.copy(mydest) for f in mylist]
+
+def move(mylist, mydest):
+    [f.move(mydest) for f in mylist]
 
 def run(basedir, maxresults, scheme, minfiles = 10):
     dirfiles = {}
@@ -62,3 +66,26 @@ def run_smaller_bias(basedir, maxresults, minfiles = 10):
 def run_recent_bias_with_files(basedir, maxresults, minfiles = 10):
     f = lambda x:len(x.files())/np.log(x.ctime)
     return run(basedir, maxresults, f, minfiles)
+
+def train_test_split(source, dest, valid_pct, inplace = False):
+    p_source = path(source)
+    dirs = p_source.dirs()
+    dir_names = [str(a.name) for a in dirs]
+    path(dest).mkdir_p()
+    p_train = path(dest + "/train")
+    p_valid = path(dest + "/valid")
+    p_train.mkdir_p()
+    p_valid.mkdir_p()
+    for n in dir_names:
+        path(p_train + "/" + n).mkdir_p()
+        path(p_valid + "/" + n).mkdir_p()
+    for d in dirs:
+        files = d.files()
+        random.shuffle(files)
+        split = round(valid_pct * len(files))
+        if inplace == False:
+            operation = copy
+        else:
+            operation = move
+        operation(files[0:split], p_valid + "/" + d.name)
+        operation(files[split:], p_train + "/" + d.name)
