4f85fb1196ec132245f21ffa3ef48a41ba2c30a9
xjdeng <xjd001@gmail.com>
2019-03-25 21:34:05 -0500

    Don't import gui in __init__
    Because it require EasyWebdriver

--- Easy_Image/__init__.py
+++ Easy_Image/__init__.py
@@ -3,10 +3,8 @@ try:
     faces_in_dir, load_image, load_image_dir, EasyImageList, \
     EasyImageFileList, EasyFaceList, ImageFileList
     from .detect import NotAnImage, NotFace
-    from . import gui
 except ImportError:
     from detect import DetectorParams, EasyImage, EasyImageFile, EasyFace,\
     faces_in_dir, load_image, load_image_dir, EasyImageList, \
     EasyImageFileList, EasyFaceList, ImageFileList
     from detect import NotAnImage, NotFace
-    import gui
