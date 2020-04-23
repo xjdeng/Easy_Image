9dfd1cb5d77796663aa2db964b428a9e417414bb
Xiaojian "JJ" Deng <xjd001@gmail.com>
2020-04-22 19:04:38 -0500

    Create fasterrcnn.py

---
+++ Easy_Image/fasterrcnn.py
@@ -0,0 +1,46 @@
+import torchvision
+import torchvision.transforms as T
+from PIL import Image
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+def get_prediction(img_path, threshold=0.99):
+    if isinstance(img_path, str):
+        img = Image.open(img_path) # Load the image
+    else:
+        img = img_path
+    transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+    img = transform(img) # Apply the transform to the image
+    pred = model([img]) # Pass the image to the model
+    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+    pred_boxes = [[(int(i[0]), int(i[1]), int(i[2]), int(i[3]))] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+    pred_score = list(pred[0]['scores'].detach().numpy())
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    return pred_boxes, pred_class
+
+def get_obj(img, obj, thresh=0.99):
+    try:
+        res = get_prediction(img, thresh)
+        return [a for (a,b) in zip(res[0], res[1])  if b == obj]
+    except IndexError:
+        return []
+
+def get_people(img, thresh=0.99):
+    return get_obj(img, "person", thresh)
